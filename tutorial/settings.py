8b67bb664c8a2f5d302181ae0f57bd0e9acd5350
summerlunaa <rhgus1085@gmail.com>
2021-10-06 01:43:24 +0900

    5th-Relationships-and-Hyperlinked-APIs

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -72,6 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
+REST_FRAMEWORK={
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':10
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
